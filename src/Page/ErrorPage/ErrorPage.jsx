7ce141444bdc3a8b7da2816bb9f54e7f8bf8e6b0
shoptorshi <shoptorshiria78@gmail.com>
2023-10-21 11:13:12 +0600

    error page edited

--- src/Page/ErrorPage/ErrorPage.jsx
+++ src/Page/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1 className="text-4xl text-center mt-20">404 NOT FOUND</h1>
+        <div className="text-center">
+            <h1 className="text-4xl text-center mt-32 text-fuchsia-500">404 NOT FOUND</h1>
+            <Link className="btn px-8 mt-8 bg-fuchsia-700 text-white text-lg" to='/'>Home</Link>
         </div>
     );
 };
