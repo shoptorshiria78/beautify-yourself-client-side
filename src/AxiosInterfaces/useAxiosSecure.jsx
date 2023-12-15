6235b505c9b15c82fad2e69753ff952eef0a7590
shoptorshi <shoptorshiria78@gmail.com>
2023-12-15 11:43:03 +0600

    axios interceptor added

--- src/AxiosInterfaces/useAxiosSecure.jsx
+++ src/AxiosInterfaces/useAxiosSecure.jsx
@@ -9,6 +9,28 @@ const axiosSecure = axios.create({
 const useAxiosSecure = () => {
     const navigate = useNavigate();
     const {logOut} = useAuth()
+//use interceptor for request to add token in every request.
+    axios.interceptors.request.use(function (config) {
+       const token = localStorage.getItem('access-token');
+       config.headers.authorization = `Bearer ${token}`
+        return config;
+      }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      });
+
+    //   use interceptor for response
+    axios.interceptors.response.use(function(response){
+        return response
+    },  async(error)=> {
+       const status = error.response.status;
+    //    logout the user if have 401, 403 status code
+       if(status === 401 || status === 403){
+        await logOut();
+        navigate('/login')
+       }
+        return Promise.reject(error);
+      })
     return axiosSecure
 };
 
